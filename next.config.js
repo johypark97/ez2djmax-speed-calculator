e5123052516849c8aea0fdb44d82e2cd480955ca
johypark97 <johypark97@gmail.com>
2023-04-12 15:21:23 +0900

    Update build scripts

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const IS_PROD = process.env.NODE_ENV === 'production';
+
+const PREFIX = IS_PROD ? '.' : undefined;
+
+const nextConfig = {
+  assetPrefix: PREFIX,
+  output: 'export',
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
