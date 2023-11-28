c4f3ac21e92614f18a80f877d1cf56ba6a13821a
johypark97 <johypark97@gmail.com>
2023-11-28 20:32:14 +0900

    Deploy v0.1.3

---
+++ _next/static/XNP6gp9UoekAmD5Hs3gFR/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/125-4ac460892ff605eb.js","static/chunks/pages/index-1f8aa5a71bfd6233.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
