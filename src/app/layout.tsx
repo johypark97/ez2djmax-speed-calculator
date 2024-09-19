451d98d60f253715ae681febbbeb714e98e14a87
johypark97 <johypark97@gmail.com>
2024-09-19 23:50:02 +0900

    Add the document title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
-import type { Metadata } from 'next';
-import { ReactNode } from 'react';
 import '@/styles/globals.css';
+import { Metadata } from 'next';
+import { ReactNode } from 'react';
 
 export const metadata: Metadata = {
+  title: 'EZ2DJMAX Speed Calculator',
   description:
     'A note speed calculator for DJMAX RESPECT V and EZ2ON REBOOT : R',
 };
