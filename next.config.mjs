c9bc7273e7f5072e208ca0414c9c10a59df21c86
johypark97 <johypark97@gmail.com>
2024-10-09 10:15:57 +0000

    Prepare to replace the deploy method
    - Removed the deploy script and out directory.
    - Removed unnecessary Next.js configurations.

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
-const isProduction = process.env.NODE_ENV === 'production';
-
-const assetPrefix = isProduction ? process.env.ASSET_PREFIX : '';
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  assetPrefix: assetPrefix,
-  images: { unoptimized: true },
-  output: 'export',
-  reactStrictMode: true,
-};
+const nextConfig = {};
 
 export default nextConfig;
--- out/_next/static/chunks/app/layout-3837997c0434852f.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{4909:function(n,e,u){Promise.resolve().then(u.t.bind(u,5204,23))},5204:function(){}},function(n){n.O(0,[846,971,23,744],function(){return n(n.s=4909)}),_N_E=n.O()}]);
