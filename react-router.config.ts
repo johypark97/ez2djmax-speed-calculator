51ea9f5392e090eb6610cbe6291b139c225f693a
johypark97 <johypark97@gmail.com>
2026-03-18 15:15:37 +0900

    Add GitHub Action and setup deploy settings

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,4 +4,5 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: false,
+  basename: process.env.GITHUB_BASE_PATH ?? '/',
 } satisfies Config;
--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,5 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  base: `${process.env.GITHUB_BASE_PATH ?? ''}/`,
 });
